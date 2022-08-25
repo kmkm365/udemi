5ead10971fd1a1004b772a67f9b142bb02a0085f
bella <kmkm3655555@gmail.com>
2022-08-26 02:30:11 +0900

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+// let colors = ["red", "orange", "yellow"];
+// let number = [1, 2, 3];
+// colors.splice(1, 1);
+// colors.splice(0, 0, "rainbow");
+// console.log(colors);
+// let sort = [1, 60, 25, 799, -12, 560];
+// sort.sort();
+// console.log(sort);
+// const ll = {
+//     1: "w", 2: "s",
+//     ss: "sssss",
+// }
+
+// console.log(ll[ss]);
+
